b568a95d827ff8d90e2d3d02707d5c176b607c54
Ender Watts <103525727+ewatgar@users.noreply.github.com>
2024-01-28 15:27:06 +0100

    fixed colision pared

--- Assets/Scripts/EnemigoIA.cs
+++ Assets/Scripts/EnemigoIA.cs
@@ -1,3 +1,5 @@
+
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class EnemigoIA : MonoBehaviour
@@ -12,6 +14,13 @@ public class EnemigoIA : MonoBehaviour
     private RotadorExtremidades[] rotadores;
     private CharacterController characterController;
     public float speed = 2;
+    //--------------------------------
+    private Vector3 origin;
+    private Vector3 direction;
+    public float sphereRadius = 0.5f;
+    public float maxDistance = 0.2f;
+    public GameObject DEBUGCurrentHitObject; //para debugear
+    public float DEBUGCurrentHitDistance; //para debugear
 
     void Start()
     {
@@ -22,24 +31,25 @@ public class EnemigoIA : MonoBehaviour
 
     void Update()
     {
-        Ray rayo = new Ray(
-            transform.position + new Vector3(0, 1, 0) + transform.forward,
-            transform.forward
-        );
-
+        origin = transform.position + Vector3.up;
+        direction = transform.forward;
+        Ray rayo = new Ray(origin,direction);
+        
         RaycastHit hit;
-        bool colisionPared = Physics.SphereCast(
-            rayo,
-            0.2f,
-            out hit,
-            LayerMask.GetMask("Enemy"));
-
-        if (!colisionPared){
-            Vector3 reflexionPared = Vector3.Reflect(transform.forward,hit.normal); 
+        if (Physics.SphereCast(rayo,sphereRadius,out hit,maxDistance)){
+            DEBUGCurrentHitObject = hit.transform.gameObject;
+            DEBUGCurrentHitDistance = hit.distance;
             
-            Debug.DrawLine(transform.position, hit.point, Color.red);
-            Debug.DrawLine(hit.point, hit.normal, Color.blue);
-            Debug.DrawLine(hit.point, reflexionPared, Color.white);
+            Vector3 reflexionPared = Vector3.Reflect(transform.forward,hit.normal).normalized; 
+            /*
+            Debug.DrawLine(rayo.origin, hit.point, Color.red);
+            Debug.DrawLine(hit.point, hit.point + hit.normal, Color.blue);
+            Debug.DrawLine(hit.point, hit.point + reflexionPared, Color.white);*/
+            transform.LookAt(transform.position + reflexionPared*2);
+
+        } else{
+            DEBUGCurrentHitDistance = maxDistance;
+            DEBUGCurrentHitObject = null;
         }
 
         // --------------------------------------------
@@ -56,6 +66,11 @@ public class EnemigoIA : MonoBehaviour
                 break;
         }
 
+    }
+    private void OnDrawGizmosSelected(){
+        Gizmos.color = Color.red;
+        Debug.DrawLine(origin,origin+direction*DEBUGCurrentHitDistance);
+        Gizmos.DrawWireSphere(origin+direction*DEBUGCurrentHitDistance,sphereRadius);
     }
 
     private void IniciarAnimacion()
